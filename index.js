e97506894ec95bde145117bdaf3e95fdfb313a1b
Carol Greene <carolgreene3@gmail.com>
2018-10-22 22:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-function findMatching(drivers, name) {
-  return drivers.filter(function (driverName) {
-    return driverName.toUpperCase() === name.toUpperCase()
-  })
-}
+
+function findMatching(drivers, name) {                             //This is a string, not an object w/properties.
+  return drivers.filter(function (driverName) {                     //return the drivers array & filter it to return indv string.(call the string driverName)
+    return driverName.toUpperCase() === name.toUpperCase()          //return the driverName string to upper case that equals the name argument
+})                                                                  //to upper case
+}                                                              
+
 
 function fuzzyMatch(drivers, string) {
-  let lengthOfName = string.length
-  return drivers.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === string
-  })
+  return drivers.filter(driver => driver.startsWith(string))     //use the .startsWith()
 }
 
-function matchName(drivers, name) {
-  return drivers.filter(driver => driver.name === name)
-}
+//or
+//function fuzzyMatch(drivers, string) {                     drivers array & string
+//  let lengthOfName = string.length;                        set variable to the number of the length of the string
+//  return drivers.filter(function (driverName) {            return drivers array & filter it to return individual driver. set to driverName.
+
+//    return driverName.slice(0, lengthOfName) === string       return driverName & slice it to return letters starting a 0 index &
+
+//    return driverName.slice(0, lengthOfName) === string       return driverName & slice it to return letters starting a 0 index &
+//  })                                                          lasting for the lengthOfName variable
+//}
+
+function matchName(drivers, name) {                         //This is an object with a name property
+  return drivers.filter(driver => driver.name === name)      //return drivers array & filter it to return individual driver whose
+}                                                            //name property === the name passed in
