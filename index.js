a44d3d452b178ce822cf42d5698c7d86c3a5cb84
Carol Greene <carolgreene3@gmail.com>
2018-10-22 23:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findMatching(drivers, name) {                             //This is a s
   return drivers.filter(function (driverName) {                     //return the drivers array & filter it to return indv string.(call the string driverName)
     return driverName.toUpperCase() === name.toUpperCase()          //return the driverName string to upper case that equals the name argument
 })                                                                  //to upper case
-}                                                              
+}
 
 
 function fuzzyMatch(drivers, string) {
@@ -14,12 +14,10 @@ function fuzzyMatch(drivers, string) {
 //function fuzzyMatch(drivers, string) {                     drivers array & string
 //  let lengthOfName = string.length;                        set variable to the number of the length of the string
 //  return drivers.filter(function (driverName) {            return drivers array & filter it to return individual driver. set to driverName.
-
 //    return driverName.slice(0, lengthOfName) === string       return driverName & slice it to return letters starting a 0 index &
-
-//    return driverName.slice(0, lengthOfName) === string       return driverName & slice it to return letters starting a 0 index &
-//  })                                                          lasting for the lengthOfName variable
+//})                                                          lasting for the lengthOfName variable
 //}
+
 
 function matchName(drivers, name) {                         //This is an object with a name property
   return drivers.filter(driver => driver.name === name)      //return drivers array & filter it to return individual driver whose
